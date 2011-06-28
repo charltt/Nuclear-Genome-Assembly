576032da968f60215f5dca785012a6260f90949e
charltt <charltt@allegheny.edu>
2011-06-28 14:33:09 -0400

    Making changes to some of the scripts and adding more useful MAQ info.

--- USDAscripts/pythonscripts.sh
+++ USDAscripts/pythonscripts.sh
@@ -1,17 +1,19 @@
-#!/bin/sh
+#!/bin/bash
 
 #path to unprocessed fastq files for each lane
-dir1=/Research/Listeria.dir/N53.dir/RawData.dir/
+dir1=~/Desktop/Trichinella.dir/T1.dir/T1_RawData/Flowcell4/
+
+i=2
 
 
-i=1
 
-#for each .fastq in the directory, run quality_breakdown.py and store results in .qual 
+#for each .fastq in the directory, run quality_breakdown.py and store 		results in .qual 
 for file in `ls $dir1/s_*`
 
 do
-    /usr/local/bin/USDA/quality_breakdown.py $file > s_$i.qual
-    /usr/local/bin/USDA/eliminate_n_single.py $file > s_$i.fastq.NoNs
-    /usr/local/bin/USDA/quality_breakdown.py s_$i.fastq.NoNs > s_$i.fastq.NoNs.qual
-    let i=$i+1
+    	~/Nuclear-Genome-Assembly/USDAscripts/quality_breakdown.py $file > s_$i.qual
+    	~/Nuclear-Genome-Assembly/USDAscripts/eliminate_n_single.py $file > s_$i.fastq.NoNs
+    	~/Nuclear-Genome-Assembly/USDAscripts/quality_breakdown.py s_$i.fastq.NoNs > s_$i.fastq.NoNs.qual
+
+    	let i=$i+1
 done
